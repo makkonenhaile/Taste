2489fd01669e7de314111c9cb5d156ef3c2650e0
Damien Felch <dfelch@usc.edu>
2023-02-05 02:00:08 -0800

    Add files via upload

---
+++ taste/main.py
@@ -0,0 +1,112 @@
+import sqlite3 as sl
+from flask import Flask, render_template, request, url_for, redirect
+from flask_session import Session
+
+def get_db_connection():
+    conn = sl.connect('database.db')
+    conn.row_factory = sl.Row
+    return conn
+
+app = Flask(__name__, template_folder = 'templates', static_folder = 'static')
+app.config['SECRET_KEY'] = 'secretkey123'
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+def db_create_user(un: str, pw: str) -> bool:
+    conn = sl.connect("database.db")
+    c = conn.cursor()
+    v = (un,pw)
+    c.execute("SELECT COUNT(*) FROM users WHERE username=?",(un,))
+    result = c.fetchone()[0]
+    if result == 0:
+        return True
+    else:
+        return False
+
+
+def db_check_creds(un: str, pw: str) -> bool:
+    conn = sl.connect("database.db")
+    c = conn.cursor()
+    # getting username and password
+    v = (un,pw)
+    # getting data from
+    x = ("""SELECT * FROM users WHERE `username`=? AND `password` =?""")
+    data = c.execute(x,v)
+    conn.commit()
+    if not data:
+        #conn.close()
+        return False
+    # if there is data return True and close connection
+    for r in data:
+        #conn.close()
+        return True
+    return False
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html') 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form['password']
+        if db_check_creds(username, password) == True:
+            # User is valid, proceed to their homepage or wherever they need to go
+            return redirect('/truefeed')
+        else:
+            # User is not valid, show error message
+            return render_template('login.html', error='Invalid credentials')
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    conn = sl.connect("database.db")
+    c = conn.cursor()
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form['password']
+        confirmedPassword = request.form['password_again']
+        v = (username, password)
+        b = db_create_user(username,password)
+        if b:
+            if password == confirmedPassword:
+                stmnt1 = "INSERT INTO users (`username`, `password`) VALUES (?,?)"
+                c.execute(stmnt1, v)
+                conn.commit()
+                # session["username"] = username
+                # session["id"] = conn.execute("SELECT userID FROM users WHERE username = ", username)
+                return redirect('/truefeed')
+            else:
+                return render_template('signup.html', error='Invalid credentials')
+        else:
+            return render_template('signup.html', error='Invalid credentials')
+    return render_template("signup.html")
+
+@app.route('/truefeed')
+def truefeed():
+    conn = get_db_connection()
+    reviews = conn.execute('SELECT * FROM reviews WHERE userID != 1 ORDER BY reviewID DESC').fetchall()
+    conn.row_factory = lambda cursor, row: row[0]
+    usernames = conn.execute('''SELECT u.username
+                            FROM users u, reviews r
+                            Where u.userID = r.userID
+                            AND u.userID != 1
+                            ''').fetchall()
+    conn.close()
+    return render_template('truefeed.html', reviews=reviews, usernames=usernames) 
+
+@app.route('/trueprofile')
+def trueprofile():
+    conn = get_db_connection()
+    reviews = conn.execute('SELECT * FROM reviews WHERE userID = 1 ORDER BY reviewID DESC').fetchall()
+    conn.row_factory = lambda cursor, row: row[0]
+    usernames = conn.execute('''SELECT u.username
+                            FROM users u, reviews r
+                            Where u.userID = r.userID
+                            AND u.userID = 1
+                            ''').fetchall()
+    conn.close()
+    return render_template('trueprofile.html', reviews=reviews, usernames=usernames) 
---
+++ taste/schema.py
@@ -0,0 +1,85 @@
+import sqlite3 as sl
+
+# connect to database
+conn = sl.connect('database.db')
+
+# create a cursor
+c = conn.cursor()
+
+# c.execute ("DROP TABLE users")
+# c.execute ("DROP TABLE media")
+# c.execute ("DROP TABLE reviews")
+
+# c.execute (''' CREATE TABLE users (
+#     userID INTEGER PRIMARY KEY AUTOINCREMENT,
+#     username text NOT NULL,
+#     password text NOT NULL
+# )
+#            ''')
+
+# c.execute (''' CREATE TABLE media (
+#     mediaID INTEGER PRIMARY KEY AUTOINCREMENT,
+#     mediaName text NOT NULL,
+#     mediaType text NOT NULL
+# )
+#            ''')
+
+# c.execute (''' CREATE TABLE reviews (
+#     reviewID INTEGER PRIMARY KEY AUTOINCREMENT,
+#     userID INTEGER NOT NULL,
+#     mediaID INTEGER NOT NULL,
+#     reviewTitle text NOT NULL,
+#     reviewText text NOT NULL,
+#     mediaType text NOT NULL,
+#     yum TINYINT NOT NULL,
+#     date NOT NULL
+# )
+#            ''')
+
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh10', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh2', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh3', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh4', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh5', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh6', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh7', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh8', '1738nay')")
+# c.execute("INSERT INTO users (username, password) VALUES ('bihyuh9', '1738nay')")
+# # c.execute("SELECT * FROM users")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman1', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman2', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman3', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman4', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman5', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman6', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman7', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman8', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman9', 'movie')")
+# c.execute("INSERT INTO media (mediaName, mediaType) VALUES ('Superman10', 'movie')")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (4, 1, 'MID','not a1', 'movie', 1, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (5, 5, 'MID','a banger2', 'movie',  0, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (1, 6, 'MID', 'not banger3', 'movie',  1, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (2, 10, 'MID', 'banger not4', 'movie',  0, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (6, 1, 'MID', 'not a5', 'movie',  1, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (3, 5, 'MID', 'a banger6', 'movie',  0, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (1, 6, 'MID', 'not banger7', 'movie',  1, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (7, 10, 'MID', 'banger not8', 'movie',  0, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (1, 6, 'MID', 'not banger9', 'movie',  1, datetime('now', 'localtime'))")
+# c.execute("INSERT INTO reviews (userID, mediaID, reviewTitle, reviewText, mediaType, yum, date) VALUES (9, 10, 'MID', 'banger not10', 'movie',  0, datetime('now', 'localtime'))")
+c.execute("SELECT * FROM users")
+# c.execute('''SELECT u.username
+#                 FROM users u, reviews r
+#                 Where u.userID = r.userID
+#                 AND u.userID != 1
+#                 ''')
+for row in c:
+    print("userID = ", row[0])
+    print("username = ", row[1])
+    print("password = ", row[2])
+    
+
+# c.execute("SELECT * FROM users")
+
+conn.commit()
+
+conn.close()
